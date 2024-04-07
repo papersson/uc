8a20f67bef542d5fc2a1786c2e41f7b805eecf0a
Patrik Persson <patrik95195@gmail.com>
2024-04-07 10:18:56 +0200

    finish initial client testing suite

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,7 @@
 import os  # Add this import
 from dotenv import load_dotenv  # Add this import
 from unittest import TestCase
-from uc.databricks.http_client import CatalogClient
+from uc.databricks.http_client import CatalogClient, SecurityGroupClient
 
 load_dotenv()  # This loads the variables from .env into the environment
 
@@ -19,4 +19,21 @@ class TestCatalogClientIntegration(TestCase):
         
         # Clean up: delete the test catalog
         delete_response = self.client.delete_catalog("integration_test_catalog", force=True)
+        self.assertIn(delete_response.status_code, [200, 204])
+
+class TestSecurityGroupClientIntegration(TestCase):
+    def setUp(self):
+        access_token = os.getenv('DATABRICKS_ACCESS_TOKEN')
+        databricks_instance = os.getenv('DATABRICKS_INSTANCE_URL')
+        self.client = SecurityGroupClient(access_token, databricks_instance)
+
+    def test_create_and_delete_security_group_integration(self):
+        # Create a test security group
+        create_response = self.client.create_security_group("integration_test_group")
+        self.assertIn(create_response.status_code, [200, 201])
+        
+        group_id = create_response.json().get('id')
+        
+        # Clean up: delete the test security group
+        delete_response = self.client.delete_security_group(group_id)
         self.assertIn(delete_response.status_code, [200, 204])
